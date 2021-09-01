c716b9dad1dcb00b99f6251adae1f0a44c24771b
minsikkim2 <89857142+minsikkim2@users.noreply.github.com>
2021-09-02 01:09:57 +0900

    add C

---
+++ Grade Manager.c
@@ -0,0 +1,33 @@
+//
+//  Grade Manager.c
+//  First project
+//
+//  Created by 김민식 on 2021/09/02.
+//
+//전처리문
+#include <stdio.h>
+
+//메인함수
+int main(int argc, char*argv[])
+{
+    //선언부
+    /**
+     이름
+     등급(A~F)
+     국어점수
+     수학점수
+     영어점수
+     과학점수
+     */
+    char name[20] ="홍길동";
+    char grade = 'B';
+    int scoreKOR = 90;
+    int scoreMAT = 70;
+    int scoreENG = 80;
+    int scoreSCI = 90;
+    
+    
+    //구현부
+    
+}
+
