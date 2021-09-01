545428211668367e57d088c3042237d32014c7d9
minsikkim2 <89857142+minsikkim2@users.noreply.github.com>
2021-09-01 22:39:58 +0900

    First project c

---
+++ First project/First project/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  First project
+//
+//  Created by 김민식 on 2021/09/01.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf ("Hello world\n");
+    return 0;
+}
